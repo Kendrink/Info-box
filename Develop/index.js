d9977c115a4984ddbba90e10fd0bc6072569411f
Kendrink <you@example.com>
2024-03-16 10:38:03 -0700

    added the packages for this app

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 
 
