a271f08e2c9169ded3e40017c0f46aa823506f39
Kendrink <you@example.com>
2024-03-16 11:47:40 -0700

    added the questions for inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -7,7 +7,40 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const InquirerQuestions = [
+
+       
+        {
+          type: 'input',
+          message: 'Your-Project-Title?',
+          name: 'project title',
+        },
+        {
+            type: 'input',
+            message: 'What was your motivation?',
+            name: 'motivation',
+          },
+        {
+          type: 'input',
+          message: 'Provide a short description explaining the what, why, you built this project?',
+          name: 'Description',
+        },
+        {
+            type: 'input',
+            message: 'What problem does it solve?',
+            name: 'solution',
+          },
+          {
+            type: 'input',
+            message: 'What are the steps required to install your project?',
+            name: 'installation process',
+          },
+          {
+            type: 'input',
+            message: 'what are the features of this app?',
+            name: 'features',
+          },
+      ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
