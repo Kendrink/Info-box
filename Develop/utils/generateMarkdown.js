51daf03b992171dfb7d785fbacbcbd4a24665a3c
Kendrink <you@example.com>
2024-03-18 00:32:37 -0700

    final commit, used prettier for a better view

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,71 +1,33 @@
-// TODO: Create a function that 
+// TODO: Create a function that
 //returns a license badge based on which license is passed in
-
 
 // / TODO: Create a function that returns the license link
 // // If there is no license, return an empty string
 // function renderLicenseLink(license) {}
 function LicenseBagde(data) {
-  
   if (data.license === "Eclipse Public License 1.0") {
-    badge = `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`
+    badge = `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
+  } else if (data.license === `GNU GPL v3`) {
+    badge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (data.license === `GNU GPL v2`) {
+    badge = `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
+  } else if (data.license === `GNU AGPL v3`) {
+    badge = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
+  } else if (data.license === `GNU LGPL v3`) {
+    badge = `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`;
+  } else if (data.license === `GNU FDL v1.3`) {
+    badge = `[![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)`;
+  } else {
+    badge = "";
   }
-  
-  else if (data.license === `GNU GPL v3`) {
-    badge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
-  } 
-  
-  else if (data.license === `GNU GPL v2`) {
-    badge = `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
-  }
-  
-  else if (data.license === `GNU AGPL v3`) {
-    badge = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`
-  } 
-  
-  else if (data.license === `GNU LGPL v3`) {
-    badge = `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`
-  }
-  
-  else if (data. license === `GNU FDL v1.3`) {
-    badge = `[![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)`}
-
-    else {
-      badge = ''
-} 
-};
-  
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
 
 
+  
 }
-
-
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -73,8 +35,8 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-    LicenseBadge(data)
-    return `# ${data.title}  
+  LicenseBadge(data);
+  return `# ${data.title}  
     # Description
     ${data.description}
     
